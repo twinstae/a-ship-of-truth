7b4405cc849907ee76c286baf0ef125216a63e82
Aditya Telange <21258296+adityatelange@users.noreply.github.com>
2025-02-01 15:04:31 +0530

    Add project files

---
+++ astro.config.mjs
@@ -0,0 +1,55 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+
+import starlightThemeObsidian from 'starlight-theme-obsidian';
+import { createStarlightObsidianPlugin } from 'starlight-obsidian';
+
+const [vault1_StarlightObsidian, vault1_ObsidianSidebarGroup] = createStarlightObsidianPlugin()
+const [vault2_StarlightObsidian, vault2_ObsidianSidebarGroup] = createStarlightObsidianPlugin()
+
+// https://astro.build/config
+export default defineConfig({
+	integrations: [
+		starlight({
+			title: 'My Notes',
+			social: {
+				gitlab: 'https://gitlab.com/adityatelange/astro-obsidian-starlight-notes-template',
+			},
+			customCss: [
+				// Relative path to your custom CSS file
+				'./src/styles/custom.css',
+			],
+			sidebar: [
+				{
+					label: 'Guides',
+					items: [
+						// Each item here is one entry in the navigation menu.
+						{ label: 'Example Guide', slug: 'guides/example' },
+					],
+				},
+				{
+					label: 'Reference',
+					autogenerate: { directory: 'reference' },
+				},
+				vault1_ObsidianSidebarGroup,
+				vault2_ObsidianSidebarGroup
+			],
+			plugins: [
+				// Set theme
+				starlightThemeObsidian(),
+				// Generate the Obsidian vault pages.
+				vault1_StarlightObsidian({
+					vault: 'obsidian-vaults/vault1',
+					output: 'notes-vault1',
+					sidebar: { label: 'Vault One', collapsedFolders: true },
+				}),
+				vault2_StarlightObsidian({
+					vault: 'obsidian-vaults/vault2',
+					output: 'notes-vault2',
+					sidebar: { label: 'Vault Two', collapsedFolders: true },
+				}),
+			]
+		}),
+	],
+});
